ebbcdd081bbead3d6a68f6989c297b8a1ef6db6b
AyanaZaire <ayanazaire@gmail.com>
2019-02-18 12:10:04 -0500

    photo upload to edit member

--- src/components/Member.js
+++ src/components/Member.js
@@ -292,12 +292,12 @@ class Member extends Component {
                     fluid label='Website'
                     placeholder='Website' />
                 </Form.Group>
-                  // <Form.Input
-                  //   name='img_url'
-                  //   value={this.state.value.img_url}
-                  //   onChange={this.handleChange}
-                  //   fluid label='Image Url'
-                  //   placeholder='Include the image url that will be your profile photo' />
+                  <Form.Input
+                    name='img_url'
+                    value={this.state.value.img_url}
+                    onChange={this.handleChange}
+                    fluid label='Image Url'
+                    placeholder='Include the image url that will be your profile photo' />
                     <Form.Input
                       name='img_upload'
                       type= 'file'
