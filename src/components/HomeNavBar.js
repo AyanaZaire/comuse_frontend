3b8286b5b021145bdf0bfd87b54ed015c15ae5c0
AyanaZaire <ayanazaire@gmail.com>
2019-02-18 11:37:49 -0500

    debugging photos in prod enviorment

--- src/components/HomeNavBar.js
+++ src/components/HomeNavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Button, Image } from 'semantic-ui-react'
 
-const HOST_URL = 'https://comuse-backend.herokuapp.com/rails'
+const HOST_URL = 'https://comuse-backend.herokuapp.com'
 // const HOST_URL = 'http://localhost:3000'
 
 const HomeNavBar = props => {
